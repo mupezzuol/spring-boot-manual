9579ed685e94712d418ab8be6fdc9e18d4d623e5
mupezzuol <murillopezzuol@hotmail.com>
2019-04-17 19:01:25 -0300

    Anotacoes; Ajuste nos comentarios

--- src/main/java/br/com/springboot/spring_boot_manual/controller/HomeController.java
+++ src/main/java/br/com/springboot/spring_boot_manual/controller/HomeController.java
@@ -6,10 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 	
-	//Precisamos importar a 'engine de templates' nesse caso 'thymeleaf' para encontrar nosso padronização do spring boot
+	//Precisamos importar a 'engine de templates' nesse caso 'thymeleaf' pois ele pega o template e faz a configuração necessária
+	//para que possamos ver nossas páginas no navegador.. 
+	//Colocamos o 'spring-boot-starter-thymeleaf' para configurar para nós, é o papel do 'engine templates' fazer isso
 	@RequestMapping("/")
     public String index(){
-        return "index";//Usando o padrão do spring boot ele já sabe onde buscar a página em 'templates'
+        return "index";
     }
 	
 }
