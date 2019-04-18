14a20b6f5df09a1f6cc03a6a810382474913fd4d
mupezzuol <murillopezzuol@hotmail.com>
2019-04-18 15:47:56 -0300

    application.properties; configs pelo application de datasource e server

--- src/main/java/br/com/springboot/spring_boot_manual/SpringBootInicializacao.java
+++ src/main/java/br/com/springboot/spring_boot_manual/SpringBootInicializacao.java
@@ -1,12 +1,7 @@
 package br.com.springboot.spring_boot_manual;
 
-import javax.sql.DataSource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
 
 @SpringBootApplication
 public class SpringBootInicializacao {
@@ -14,16 +9,6 @@ public class SpringBootInicializacao {
 	//Inicialização do Spring Boot
     public static void main(String[] args){
         SpringApplication.run(SpringBootInicializacao.class, args);
-    }
-    
-    @Bean //Ser reconhecido como um Bean do Spring, para gerencia-lo
-    public DataSource dataSource(){
-    	DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUsername("root");
-        dataSource.setPassword("postgres");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/spring_boot_manual");
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-		return dataSource;
     }
     
 }
